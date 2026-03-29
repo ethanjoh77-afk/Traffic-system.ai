7c52eefd63d594938d1cf83e890cef8f27989a73
trigger <triggerbeatz7@gmail.com>
2026-03-28 18:08:28 -0700

    remove login for demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import API_URL from "./config";
 import Payment from "./components/Payment.jsx";
+import API_URL from "./config";
 
 function App() {
   const [data, setData] = useState(null);
@@ -9,42 +9,27 @@ function App() {
     fetch(`${API_URL}/stats`)
       .then(res => res.json())
       .then(data => setData(data))
-      .catch(err => console.log("API ERROR:", err));
+      .catch(err => console.log(err));
   }, []);
 
   return (
-    <div style={{
-      padding: "20px",
-      fontFamily: "Arial",
-      backgroundColor: "#0f172a",
-      color: "white",
-      minHeight: "100vh"
-    }}>
-      
-      <h1>🚦 Smart Traffic AI System</h1>
-      <p>LIVE DASHBOARD</p>
+    <div style={{ padding: "20px" }}>
+      <h1>🚦 Traffic AI System (LIVE)</h1>
 
       <Payment />
 
-      <hr />
-
-      <h2>📊 Live Traffic Data</h2>
+      <h2>Live Data:</h2>
 
       {data ? (
-        <div style={{
-          background: "#1e293b",
-          padding: "15px",
-          borderRadius: "10px"
-        }}>
-          <p>🚗 Cars: {data.cars}</p>
-          <p>🚌 Buses: {data.buses}</p>
-          <p>🚚 Trucks: {data.trucks}</p>
-          <p>🚶 People: {data.people}</p>
+        <div>
+          <p>Cars: {data.cars}</p>
+          <p>Buses: {data.buses}</p>
+          <p>Trucks: {data.trucks}</p>
+          <p>People: {data.people}</p>
         </div>
       ) : (
-        <p>Loading data...</p>
+        <p>Loading...</p>
       )}
-
     </div>
   );
 }
